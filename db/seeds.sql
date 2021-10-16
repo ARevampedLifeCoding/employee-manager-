c14ed1ed6ee02fb37a4fc17118ad2acf8ac6ca64
Randy Langston <arevampedlifecoding@gmail.com>
2021-10-16 15:43:58 -0400

    added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES ('Tech Support'),
+('Software Intern'),
+('Software Engineer'),
+('Management'),
+('Zar');
+
+
+INSERT INTO postion (title,department_id)
+VALUES ('Tech Support',1),
+('Software Intern',2),
+('Software Engineer',3),
+('Management',4),
+('Zar',5);
+
+INSERT INTO employee (first_name,last_name,postion_id)
+VALUES ('Randy','Langston',5),
+('Stephenie','Langston',4),
+('Auggie','Langston',3),
+('Ally','Langston',2),
+('Jackson','Langston',1);
