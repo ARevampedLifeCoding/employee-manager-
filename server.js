4f794cfcbc0f9338fb703fc83bfc2b9e3314edb0
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-09 12:09:00 -0500

    added the prompt function

--- server.js
+++ server.js
@@ -1,28 +1,60 @@
-require("dotenv").config()
-const db = require("./db")
-const inquirer = require("inquirer")
-const consoleTable = require("console.table")
-const database = require("./db/connections")
+require("dotenv").config();
+const db = require("./db");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const database = require("./db/connections");
 
-const config = require("./package.json")
+const config = require("./package.json");
 
 const prompt = () => {
   inquirer
-  .prompt({
-    name: "choices",
-    type: "list",
-    message: "Choose an option",
-    options: [
-      "View employee",
-      "View department",
-      "View position",
-      "Add employee",
-      "Add department",
-      "Add position",
-      "Delete employee",
-      "Delete department",
-      "Delete position",
-      "Quit"
-    ]
-  })
-}
+    .prompt({
+      name: "choices",
+      type: "list",
+      message: "Choose an option",
+      options: [
+        "View employee",
+        "View department",
+        "View position",
+        "Add employee",
+        "Add department",
+        "Add position",
+        "Delete employee",
+        "Delete department",
+        "Delete position",
+        "Quit",
+      ],
+    })
+    .then((select) => {
+      switch (select.options) {
+        case "View employee":
+          viewEmployee();
+          break;
+        case "View department":
+          viewDepartment();
+          break;
+        case "View position":
+          viewPosition();
+        case "Add employee":
+          addEmployee();
+          break;
+        case "Add department":
+          addDepartment();
+          break;
+        case "Add position":
+          addPosition();
+          break;
+        case "Delete employee":
+          deleteEmployee();
+          break;
+        case "Delete department":
+          deleteDepartment();
+          break;
+        case "Delete position":
+          break;
+          default: "Quit"
+      }
+    });
+};
+
+prompt()
