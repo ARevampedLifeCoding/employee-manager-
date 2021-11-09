6d65131e8d1e4b99a605d2b36908a4d25dffd7b5
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-09 11:59:46 -0500

    added options to chose from in server

--- server.js
+++ server.js
@@ -3,3 +3,26 @@ const db = require("./db")
 const inquirer = require("inquirer")
 const consoleTable = require("console.table")
 const database = require("./db/connections")
+
+const config = require("./package.json")
+
+const prompt = () => {
+  inquirer
+  .prompt({
+    name: "choices",
+    type: "list",
+    message: "Choose an option",
+    options: [
+      "View employee",
+      "View department",
+      "View position",
+      "Add employee",
+      "Add department",
+      "Add position",
+      "Delete employee",
+      "Delete department",
+      "Delete position",
+      "Quit"
+    ]
+  })
+}
