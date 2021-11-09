6b5b9a59940c3904179b03259467f89e05af34bb
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-09 11:24:48 -0500

    working on getting the server to start agiain

---
+++ server.js
@@ -0,0 +1,61 @@
+const inquirer = require('inquirer');
+const prompts = require('./query/prompts.js');
+
+let restart = true;
+
+const questions = () => {
+  inquirer.prompt(
+    [
+      {
+        type: 'list',
+        name: 'nextMove',
+        choices: [
+                    'View All Employees', 
+                    'Add Employee', 
+                    'Update Employee Role', 
+                    'View All Roles', 
+                    'Add Role', 
+                    'View All Departments', 
+                    'Add Department', 
+                    'Quit'
+                  ],
+        message: "What would you like to do?"
+      }
+    ]
+  ).then(async response => {
+    switch(response.nextMove) {
+      case "View All Employees": 
+        await prompts.viewEmployees()
+        break;
+      case "Add Employee": 
+        await prompts.addEmployee();
+        break;
+      case "Update Employee Role": 
+        await prompts.updateRole();
+        break;
+      case "View All Roles": 
+        await prompts.viewRoles();
+        break;
+      case "Add Role": 
+        await prompts.addRole();
+        break;
+      case "View All Departments": 
+        await prompts.viewDepartments();
+        break;
+      case "Add Department": 
+        await prompts.addDepartment();
+        break;
+      case "Quit": 
+        console.log("Exiting program. Goodbye!");
+        restart = false; 
+        break;
+    }
+    if (restart) {
+      init();
+    } else {
+      prompts.endConnection();
+    }
+  });
+};
+
+questions();
